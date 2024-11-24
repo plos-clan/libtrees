49b986a8d570e0b7d95c9ecc89a80fa0333ebb96
SegmentationFaultCD <3076253106@qq.com>
2024-11-24 14:32:34 +0800

    wirte test file

--- test/main.cpp
+++ test/main.cpp
@@ -1,6 +1,15 @@
-#include <iostream>
-
-int main(int argc, char** argv) {
-    std::cout << "hello world!" << std::endl;
-    return 0;
+#include "../include/rbtree.hpp"
+#include <print>
+#include <ranges>
+auto main( void ) -> int {
+    using namespace libtrees;
+    RedBlackTree< int, int > a { };
+    for ( auto i : std::views::iota( 1, 100 ) ) {
+        a.insert( *new RedBlackTree< int, int >::RBTreeNode( i, new int( i ) ) );
+    }
+    std::println( "{}", *a.search( 3 )->_data );
+    std::println( "{}", *a.search( 4 )->_data );
+    for ( auto &it : a ) {
+        std::println( "{}", (int)it );
+    }
 }
--- xmake.lua
+++ xmake.lua
@@ -1,8 +1,8 @@
 add_rules("mode.debug", "mode.release")
-
+set_languages("c++26")
 target("libtrees")
     set_kind("binary")
-    add_files("src/*.cpp")
+    add_files("test/*.cpp")
 
 --
 -- If you want to known more usage about xmake, please see https://xmake.io
